5ec51fe1ea2cbd67644a9bf669693ebdbd898e37
tauqeer992 <tauqeer992@gmail.com>
2026-02-12 17:20:31 +0500

    update

--- pro_dashboard.py
+++ pro_dashboard.py
@@ -84,7 +84,7 @@ def load_data(symbol, timeframe):
         interval = "60m"
     elif timeframe == "1 Week":
         period = "7d"
-        interval = "1h"
+        interval = "4h"
     elif timeframe == "1 Month":
         period = "1mo"
         interval = "1h"
